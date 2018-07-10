6a971adb8629def71e52ac393eb4abebe94c3d9f
Alejandro Lazo Hernandez <Alejandro.Lazo@openlink.com>
2018-07-10 11:54:55 -0400

    Add ZipCodeData Data Contract and IGeoService Service Contract

---
+++ GeoLib.Contracts/IGeoService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GeoLib.Contracts
+{
+	[ServiceContract]
+	public interface IGeoService
+	{
+		[OperationContract]
+		ZipCodeData GetZipInfo(string zip);
+
+		[OperationContract]
+		IEnumerable<string> GetStates(bool primaryOnly);
+
+		[OperationContract(Name = "GetZipsByState")]
+		IEnumerable<ZipCodeData> GetZips(string state);
+
+		[OperationContract(Name ="GetZipsByRange")]
+		IEnumerable<ZipCodeData> GetZips(string state, int range);
+	}
+}
