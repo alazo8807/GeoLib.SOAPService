551f26ccb83c0a3eba73427674328fc783d35e14
Alejandro Lazo Hernandez <Alejandro.Lazo@openlink.com>
2018-07-16 12:52:47 -0400

    Add Proxy class

---
+++ GeoLib.Proxies/GeoClient.cs
@@ -0,0 +1,29 @@
+﻿using GeoLib.Contracts;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GeoLib.Proxies
+{
+	public class GeoClient : ClientBase<IGeoService>, IGeoService
+	{
+		public IEnumerable<string> GetStates(bool primaryOnly)
+		{
+			return Channel.GetStates(primaryOnly);
+		}
+
+		public ZipCodeData GetZipInfo(string zip)
+		{
+			return Channel.GetZipInfo(zip);
+		}
+
+		public IEnumerable<ZipCodeData> GetZips(string state)
+		{
+			return Channel.GetZips(state);
+		}
+
+		public IEnumerable<ZipCodeData> GetZips(string state, int range)
+		{
+			return Channel.GetZips(state, range);
+		}
+	}
+}
