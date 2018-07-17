5349f3017eb9cd220268911470d84ba4500db743
Alejandro Lazo Hernandez <Alejandro.Lazo@openlink.com>
2018-07-17 08:23:13 -0400

    Add GetInfo call on the client end

--- GeoLib.Client/MainWindow.xaml.cs
+++ GeoLib.Client/MainWindow.xaml.cs
@@ -1,41 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GeoLib.Contracts;
+using GeoLib.Proxies;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Windows;
 
 namespace GeoLib.Client
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId +
-                " | Process " + Process.GetCurrentProcess().Id.ToString();
-        }
+			this.Title = "UI Running on Thread " + Thread.CurrentThread.ManagedThreadId +
+				" | Process " + Process.GetCurrentProcess().Id.ToString();
+		}
 
-        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtZipCode.Text != "")
-            {
+		private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+		{
+			if (txtZipCode.Text != "")
+			{
+				GeoClient proxy = new GeoClient();
 
-            }
-        }
+				ZipCodeData data = proxy.GetZipInfo(txtZipCode.Text);
+				if (data != null)
+				{
+					lblCity.Content = data.City;
+					lblState.Content = data.State;
+				}
+			}
+		}
 
-        private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtState.Text != null)
-            {
+		private void btnGetZipCodes_Click(object sender, RoutedEventArgs e)
+		{
+			if (txtState.Text != null)
+			{
 
-            }
-        }
+			}
+		}
 
-        private void btnMakeCall_Click(object sender, RoutedEventArgs e)
-        {
+		private void btnMakeCall_Click(object sender, RoutedEventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
